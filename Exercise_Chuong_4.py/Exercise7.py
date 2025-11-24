c68a0f3c1b9e588e769a46b78e4b136175ad779c
HoangMinh-TDH <hoangminh240306@gmail.com>
2025-11-24 23:13:36 +0700

    Add files via upload

---
+++ Exercise_Chuong_4.py/Exercise7.py
@@ -0,0 +1,20 @@
+in_score=input("Enter Score: ")
+def computegrade(in_score):
+   try:
+      score=float(in_score)
+      if 0.0 <= score <= 1.0:
+         if score > 0.9:
+            print("A")
+         elif score > 0.8:
+            print("B")
+         elif score > 0.7:
+            print("C")
+         elif score > 0.6:
+            print("D")
+         else:
+            print("F")
+      else:
+         print("Bad score") 
+   except ValueError:
+      print("Bad score")  
+computegrade(in_score)
