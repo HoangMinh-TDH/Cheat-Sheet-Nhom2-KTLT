988cde20b317223bacc208e0b3b75ad38907cd57
HoangMinh-TDH <hoangminh240306@gmail.com>
2025-11-24 23:10:51 +0700

    Add files via upload

---
+++ Exercise_Chuong_2.py/Exercise3.py
@@ -0,0 +1,7 @@
+a=input("Enter Hours:")
+b=input("Enter Rate:")
+hours=float(a)
+rate=float(b)
+pay=hours*rate
+pay_rounded=round(pay,2)
+print("Pay:",pay_rounded)
---
+++ Exercise_Chuong_2.py/Exercise5.py
@@ -0,0 +1,4 @@
+Celsius=float(input("Cho toi biet nhiet do thang do Celsius hien tai:"))
+Fahrenheit=(Celsius*1.8)+32
+f=round(Fahrenheit,2)
+print("Nhiet do hien tai theo thang Fahrenheit:",f)
