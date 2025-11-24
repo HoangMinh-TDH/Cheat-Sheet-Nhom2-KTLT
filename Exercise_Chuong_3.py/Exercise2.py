81d1f83ff7124e422bfffa174eaa3345751d8e69
HoangMinh-TDH <hoangminh240306@gmail.com>
2025-11-24 23:12:27 +0700

    Add files via upload

---
+++ Exercise_Chuong_3.py/Exercise2.py
@@ -0,0 +1,13 @@
+a=input("Enter Hours: ")
+try:
+    hours=float(a)
+except:
+    print("Error, please enter numeric input")
+    a1=input("Enter Hours: ")
+b=input("Enter Rate: ")
+try:
+    rate=float(b)
+except:
+    print("Error, please enter numeric input")
+    b1=input("Enter Rate: ")
+    
---
+++ Exercise_Chuong_3.py/Exercise3.py
@@ -0,0 +1,17 @@
+score_input = input("Enter score: ")
+try:
+    score = float(score_input)
+    if score < 0.0 or score > 1.0:
+        print("Bad score")
+    elif score >= 0.9:
+        print("A")
+    elif score >= 0.8:
+        print("B")
+    elif score >= 0.7:
+        print("C")
+    elif score >= 0.6:
+        print("D")
+    elif score < 0.6:
+        print("F")
+except ValueError:
+    print("Bad score")
